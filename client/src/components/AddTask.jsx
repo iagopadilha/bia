9db75ade73437450fd4192edb3dbc2cdb4d7bd80
iagopadilha <iagopadilhadev@gmail.com>
2026-05-24 09:36:20 +0000

    another one

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar tarefa gugu yeye let the world explide
+        Adicionar versao para rollback
       </button>
       
       <Modal
